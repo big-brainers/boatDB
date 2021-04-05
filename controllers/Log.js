bc348b105af0987b441938c6abb7868c3d9cb519
kccrtv <kimcincocreative@gmail.com>
2021-04-05 00:21:22 -0700

    resolve log controller typos

--- controllers/Log.js
+++ controllers/Log.js
@@ -7,60 +7,26 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
 	Log.find({})
 		.then((entry) => res.json(entry))
-		.catch(next)
-})
+		.catch(next);
+});
 
 //GET a log by id
 router.get('/:id', (req, res, next) => {
 	Log.findById({ _id: req.params.id })
 		.then((entry) => res.json(entry))
-		.catch(next)
-})
+		.catch(next);
+});
 
 //POST create a log
-router.post('/:userId/create', (req, res, next) => {
-	Log.create(req.body)
-		.then((newLog) => {
-			User.findOneAndUpdate(
-				req.params.userId,
-				{ $push: {entries: newLog} },
-				{ new: true }
-			)
-			.then(add => res.json(add))
-		})
-		.catch(next)
-})
-
-//PUT updates a log
-// router.put('/:userId/:id', (req, res, next) => {
-// 	Log.findByIdAndUpdate(
-// 			{ _id: req.params.id }, 
-// 			req.body, 
-// 			{ new: false }
-// 		)
-// 		.then((entry) => res.json(entry))
-// 		.catch(next)
-// })
-// router.patch('/:id', (req, res, next) => {
-// 	const id = req.params.id;
-// 	const entrieData = req.body;
-// 	User.findOne({
-// 	  'entries._id': id,
-// 	})
-// 	  .then((user) => {
-// 		const entry = user.entries.id(id);
-// 		entry.set(entrieData);
-// 		return user.save();
-// 	  })
-// 	  .then(() => res.sendStatus(204))
-// 	  .catch(next);
-//   })
+router.post('/', (req, res) => {
+	Log.create(req.body).then((entry) => res.json(entry));
+});
 
 //DELETE a log
 router.delete('/:userId/:id', (req, res, next) => {
 	Log.findByIdAndDelete(req.params.id)
 		.then((delEntry) => res.json(delEntry))
-		.catch(next)
-})
+		.catch(next);
+});
 
 module.exports = router;
